bc199c8e4ae1e3330a87c4707e23fc01919bae49
daunniy <d_a_u_n@naver.com>
2024-08-07 14:02:58 +0900

    player 초기설정
    테스트 수정 전 프로토타입 상태

---
+++ music_list.js
@@ -0,0 +1,5 @@
+const musicList = [
+    { name:"Dattom", artist:"Yerin Beak", audio:"Alone", img:"Datoom", total:"04:05" },
+    { name:"Mahidevran", artist:"Mahidevran rock band", audio:"Mahidevran-Maze_of_sorrow", img:"album2", total:"06:28" },
+    { name:"No Rest Or Endless Rest", artist:"Lisofv", audio:"No_Rest_Or_Endless_Rest", img:"album3", total:"04:33" },
+  ]
